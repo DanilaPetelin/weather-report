7e24eec7cb8e031b0d7d44aa1f0d683362c8d9d2
Alex Sadikov <sav1982@inbox.ru>
2019-05-25 00:34:15 +0300

    add icon

--- weather-report/src/components/TodayWeather.js
+++ weather-report/src/components/TodayWeather.js
@@ -2,14 +2,17 @@ import React from "react";
 
 class TodayWeather extends React.Component {
   render() {
+    const img = `https://openweathermap.org/img/w/${this.props.icon}.png`;
     return (
       <div>
         {this.props.city && (
           <div>
-            <p>Город: {this.props.city}</p>
+            <h1>{this.props.city}
+            <img src={img} alt="icon"/>
+            </h1>
             <p>Температура: {this.props.temp}</p>
             <p>Давление: {this.props.pressure*0.75}</p>
-            <p>Влажность: {this.props.humidity}</p>
+            <p>Влажность: {this.props.humidity}%</p>
           </div>
         )}
       </div>
